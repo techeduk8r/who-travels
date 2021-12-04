69714852f3eb9e005fd5bbf3b3d9cef564bae53a
Laura <90943940+lala-laura@users.noreply.github.com>
2021-12-04 14:15:01 -0600

    merge updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,9 @@
-<<<<<<< HEAD
 //Open Mobile Menu
-const openMenu = document.querySelector('.hamburger-icon')
-const menu = document.querySelector('.menu')
+const openMenu = document.querySelector('.hamburger-icon');
+const menu = document.querySelector('.menu');
 
-openMenu.addEventListener('click', mobileMenu)
+openMenu.addEventListener('click', mobileMenu);
 
 function mobileMenu() {
-  menu.classList.toggle('active')
-=======
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName('myslides');
-  var dots = document.getElementsByClassName('dot');
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
->>>>>>> 67bb24dd11d26275d37156f040f3e5e7880e8f2b
+  menu.classList.toggle('active');
 }
